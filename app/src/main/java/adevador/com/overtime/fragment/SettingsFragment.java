125ac44ddccb3b2227054de119c1b6d12d8af8d2
jdruwe <druwe.jeroen@gmail.com>
2014-10-22 20:29:48 +0200

    Added an about section

--- app/src/main/java/adevador/com/overtime/fragment/SettingsFragment.java
+++ app/src/main/java/adevador/com/overtime/fragment/SettingsFragment.java
@@ -1,7 +1,11 @@
 package adevador.com.overtime.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 
 import adevador.com.overtime.R;
 
@@ -14,5 +18,29 @@ public class SettingsFragment extends PreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.settings);
+
+        Preference aboutPreference = findPreference("about_key");
+        aboutPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                showDialog();
+                return true;
+            }
+        });
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.about);
+        builder.setMessage(R.string.about_text)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        // Create the AlertDialog object and return it
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
